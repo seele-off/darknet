99654e920550eb8bca5daa8629cf710f08b5492a
ゼーレ <131597070+seele-off@users.noreply.github.com>
2024-05-11 17:59:58 +0500

    Update common.ts

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -1,2 +1,5 @@
 /* eslint-disable import/prefer-default-export */
-export const PHONE_IMAGE = '/images/phone.webp';
+export const default_img_1 = '/images/default_interface_1.webp';
+export const default_img_2 = '/images/default_interface_2.webp';
+export const exclusive_img_1 = '/images/exclusive_interface_1.webp';
+export const exclusive_img_2 = '/images/exclusive_inyerface_2.webp';
