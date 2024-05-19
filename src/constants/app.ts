6953cf5a873226c78331c769d381073bb5412348
Vlad Frolov <vladfrolov.dev@gmail.com>
2024-05-20 01:43:49 +0700

    update app versions

--- src/constants/app.ts
+++ src/constants/app.ts
@@ -1,39 +1,58 @@
 import { AppChangelog } from '@interfaces/common';
 
 import {
-  default_img_1, default_img_2, exclusive_img_1, exclusive_img_2,
+  default_img_1,
+  default_img_2,
+  exclusive_img_1,
+  exclusive_img_2,
 } from '@constants/common';
 
-// eslint-disable-next-line import/prefer-default-export
 export const APP_VERSIONS: Array<AppChangelog> = [
   {
     appName: 'Default',
     descrition: 'Простой мод без каких-либо дополнительных функций.',
-    images: [default_img_1, default_img_2],
+    images: [
+        default_img_1,
+        default_img_2,
+      ],
     changelogs: [{
-      supportAndroidVersion: '9+',
-      version: '1.7.4',
-      download: '', // ссылка на apk
-      date: '29.02.24',
+      version: '1.0',
+      date: '20.05.24',
       isCurrentVersion: true,
-      changes: ['Без рекламы', 'Kodik без рекламы и доступен в разрешении 720p', 'Весь мусор внутри приложения удален', 'Monet Иконки'],
+      supportAndroidVersion: '9+',
+      download: 'https://github.com/seele-off/anixart/releases/download/anixart-default/Anixart-Default-v1.0-by-Seele.apk', // ссылка на apk
+      changes: [
+        'Без рекламы',
+        'Корона в профиле 👑',
+        'Добавлена тематическая иконка',
+        'Добавлено новое расширение "MD Seele v3.0"',
+        'Расширение предназначено для просмотра запрещенных аниме',
+        'Kodik без рекламы и доступен в разрешении 720p',
+        'Весь мусор внутри приложения удален', 'Monet Иконки',
+      ],
     }],
   },
   {
     appName: 'Exclusive',
     descrition: 'Эксклюзивный мод, особенностью этого мода является Monet Тема (Material You)',
-    images: [exclusive_img_1, exclusive_img_2],
+    images: [
+        exclusive_img_1,
+        exclusive_img_2
+      ],
     changelogs: [{
-      version: '1.5',
-      supportAndroidVersion: '9+',
-      download: '', // ссылка на apk
-      date: '11/01/23',
+      version: '1.0',
+      date: '20/05/24',
       isCurrentVersion: true,
-      changes: ['1фффффффффф фффффффффф ффффффффффффффффф',
-        '2фффффф ффффффффф', '3фффффффффффффффффффффффффффф',
-        '1фффффффффф фффффффффф ффффффффффффффффф',
-        '1фффффффффф фффффффффф ффффффффффффффффф',
-        '1фффффффффф фффффффффф ффффффффффффффффф'],
+      supportAndroidVersion: '12+',
+      download: 'https://github.com/seele-off/anixart/releases/download/anixart-exclusive/Anixart-Exclusive-v1.0-by-Seele.apk', // ссылка на apk
+      changes: [
+        'Без рекламы',
+        'Корона в профиле 👑',
+        'Monet Theme (Material You)',
+        'Добавлена тематическая иконка',
+        'Добавлено новое расширение "MD Seele v3.0"',
+        'Расширение предназначено для просмотра запрещенных аниме'
+      ],
     }],
   },
   {
@@ -41,12 +60,12 @@ export const APP_VERSIONS: Array<AppChangelog> = [
     descrition: 'Наш исследовательский мод, в котором может случиться немыслимое',
     images: [],
     changelogs: [{
-      version: '1.5',
-      download: '', // ссылка на apk
-      supportAndroidVersion: '9+',
-      date: '11/01/23',
+      version: '1.0',
+      date: '20/05/24',
       isCurrentVersion: true,
-      changes: ['1', '2', '3'],
+      supportAndroidVersion: '6+',
+      download: '', // ссылка на apk
+      changes: [],
     }],
   },
 ];
