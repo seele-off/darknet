75ee81aa988c123ebb89449f34704ae734c24e2a
Vlad Frolov <vladfrolov.dev@gmail.com>
2024-05-20 01:44:16 +0700

    fix image format

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/naming-convention */
-/* eslint-disable import/prefer-default-export */
 export const default_img_1 = '/images/default_interface_1.webp';
 export const default_img_2 = '/images/default_interface_2.webp';
 export const exclusive_img_1 = '/images/exclusive_interface_1.webp';
