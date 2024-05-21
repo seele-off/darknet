523bddbcbe27c54e2ba0126611a83d5be68e1f01
Vlad Frolov <vladfrolov.dev@gmail.com>
2024-05-21 20:09:09 +0700

    update logic

--- src/pages/api/extension/profile/[userId].ts
+++ src/pages/api/extension/profile/[userId].ts
@@ -26,37 +26,38 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   try {
     const anixartRes = await axios.get<ProfileResponse>(anixartAPI);
 
-    const modifiedProfile = anixartRes.data.profile
+    const currentProfile = anixartRes.data.profile && userId === '790852'
     ? {
        ...anixartRes.data.profile,
         is_sponsor: true,
         is_sponsor_transferred: true,
-        sponsorshipExpires: 0,
+        sponsorshipExpires: Number.MAX_SAFE_INTEGER,
         roles: [
-          {
-        "id": 1,
-        "name": "Разработчик",
-        "color": "B656E5"            
-          },
-"id": 8,
-"name": "Редактор",
-"color": "5660E5"
-},
-{
-"id": 44,
-"name": "Anixart",
-"color": "F55E5E"
-},
-{
-"id": 5,
-"name": "Jam Club",
-"color": "E57A56"
-}
+            {
+              id: 1,
+              name: "Разработчик",
+              color: "B656E5"
+            },
+            {
+              id: 8,
+              name: "Редактор",
+              color: "5660E5"
+            },
+            {
+              id: 44,
+              name: "Anixart",
+              color: "F55E5E"
+            },
+            {
+              id: 5,
+              name: "Jam Club",
+              color: "E57A56"
+          }
         ],
        }
-    : {};
+    : anixartRes.data.profile;
 
-    res.json({ ...anixartRes.data, profile: modifiedProfile });
+    res.json({ ...anixartRes.data, profile: currentProfile });
   } catch (error) {
     console.error('Error fetching data from Anixart API:', error);
     res.status(500).json({ message: 'Internal Server Error' });
