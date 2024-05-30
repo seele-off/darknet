ba863d2b76202288e154fb9d8d610dabf4f347fd
Vlad Frolov <vladfrolov.dev@gmail.com>
2024-05-30 12:29:34 +0700

    fix score

--- src/pages/api/extension/release/[id].ts
+++ src/pages/api/extension/release/[id].ts
@@ -29,7 +29,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
     if (shikiData.length) {
       const shikiScore = shikiData[0].score;
-      const anixartScore = data.release.grade.toFixed(1);
+      const anixartScore = data.release.grade.toFixed(2);
       const combinedScore = `Anixart: ${anixartScore} • Shiki: ${shikiScore}`;
 
       res.status(200).json({
